360f32decf8dc4b0142271b4319ad8c758c099d4
masonmn03 <masonnguyen03@gmail.com>
2024-11-10 06:08:43 +0000

    testing changes

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import requests
-api_key = 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv('api_key')
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q=London&appid={api_key}"
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("API key works:", response.json())
+else:
+    print("Error:", response.status_code, response.json())
