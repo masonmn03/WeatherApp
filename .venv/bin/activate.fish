a2b988ad745f191900daa25466766ce1d4d939a1
masonmn03 <masonnguyen03@gmail.com>
2025-01-29 12:56:26 -0600

    Fixed some errors running the application

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/user/Documents/coding_projects/WeatherApp/.venv"
+set -gx VIRTUAL_ENV "/Users/user/coding_projects/WeatherApp/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
